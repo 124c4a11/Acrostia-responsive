2c3e1a72bfcee5550cc66d82513833345ff8e681
124c4a11 <124c4a11@mail.ru>
2017-05-13 18:35:23 +0300

    Equal heights for .portfolio__works-item-caption

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -3,36 +3,50 @@
 import mixitup from 'mixitup';
 import magnificPopup from '../vendor/jquery.magnific-popup.js';
 import transitionLayer from './transitionLayer';
+import equalHeights from 'jquery.equalheights';
+
+var
+  $portfolio = $('.portfolio'),
+  $portfolioWorks = $portfolio.find('.portfolio__works'),
+  $portfolioWorksItem = $portfolioWorks.find('.portfolio__works-item');
 
 var
   delay = 400;
 
 function init() {
-  transitionLayer.init();
+  if ($portfolioWorks) {
+    transitionLayer.init();
 
-  mixitup('.portfolio__works', {
-    selectors: {
-      target: '.portfolio__works-item'
+    if ($portfolioWorksItem) {
+      $portfolioWorksItem
+        .find('.portfolio__works-item-caption')
+          .equalHeights();
     }
-  });
 
-  $('.portfolio__works').magnificPopup({
-    delegate: '.portfolio__works-item-link:visible',
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    callbacks: {
-      open: function() {
-        transitionLayer.open();
-        setTimeout(showGallery, delay);
-      },
-
-      close: function() {
-        transitionLayer.close();
+    mixitup('.portfolio__works', {
+      selectors: {
+        target: '.portfolio__works-item'
       }
-    }
-  });
+    });
+
+    $('.portfolio__works').magnificPopup({
+      delegate: '.portfolio__works-item-link:visible',
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      callbacks: {
+        open: function() {
+          transitionLayer.open();
+          setTimeout(showGallery, delay);
+        },
+
+        close: function() {
+          transitionLayer.close();
+        }
+      }
+    });
+  }
 }
 
 function showGallery() {
